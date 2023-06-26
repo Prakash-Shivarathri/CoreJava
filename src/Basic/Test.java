7ada7d579ef9ff45451ef8160037749c1f869e45
Prakash Shivarathri <prakashnani7530@gmail.com>
2023-06-26 11:30:23 +0530

    Second Commit

---
+++ src/Basic/Test.java
@@ -0,0 +1,11 @@
+package Basic;
+class Test extends Thread
+{
+    public void run()
+    {
+        for(int i=0; i<5; i++)
+        {
+            System.out.println("Inside Test Thread");
+        }
+    }
+}
